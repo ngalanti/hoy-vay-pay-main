ae3f34372be805b4d9bd2a5a6f469b55a147398c
ngalanti <galanti.nehoray@gmail.com>
2025-03-11 10:51:17 +0200

    dates

--- client/src/components/charts/BarChart.jsx
+++ client/src/components/charts/BarChart.jsx
@@ -15,10 +15,17 @@ import "../../styles/Chart.css";
 import { getExpensesByDate } from "../../api/expenses";
 const MONTHS = {
   1: "January",
-  2: "Febuary",
+  2: "February",
   3: "March",
   4: "April",
-  
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
 };
 
 export const BarChart = () => {
